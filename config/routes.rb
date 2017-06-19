91fa3784e3f69e986a6076dae8b262d8a7c052b8
Johnny B <johnnyboufarhat@live.com>
2017-06-19 15:24:15 +0000

    routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
+  root to: 'pages#home'
+  
+  get 'about-me', to: 'pages#about'
 
-  get 'pages/about'
-
-  get 'pages/contact'
+  get 'contact', to: 'pages#contact'
 
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
